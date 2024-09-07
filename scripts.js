fdf25d604d696fefb8a6a9db54d9ccb24e778653
Giovanni <gigialf2006@gmail.com>
2024-09-07 13:40:58 -0500

    primer commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// Selección de la imagen del menú
+const menuImage = document.getElementById('menuImage');
+
+// Agregar un evento de clic para manejar la interacción del menú
+menuImage.addEventListener('click', () => {
+    alert('Menú activado');
+    // Aquí puedes agregar la lógica que quieras para mostrar submenús o realizar alguna acción.
+});
